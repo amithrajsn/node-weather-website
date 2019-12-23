5c5cc3339234d5c7342b397b3254864b6a96ac9f
Amith Raj <amithrajsn@gmail.com>
2019-12-23 15:28:21 +0530

    Minor UI fix

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
             callback(body.error, undefined)
         } else {
             const data = body.currently
-            const message = body.daily.data[0].summary + ' It is currently ' + data.temperature + ' degrees out. There is ' + data.precipProbability + '% chances of rain'
+            const message = body.daily.data[0].summary + ' It is currently ' + data.temperature + ' degrees out. There is ' + data.precipProbability + '% chances of rain.'
             callback(undefined, message)
         }
     })
