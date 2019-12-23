82c87e9875380e317ebf0f74ed9854faf52a4655
Amith Raj <amithrajsn@gmail.com>
2019-12-23 15:25:12 +0530

    Adding content in about page

--- public/js/app.js
+++ public/js/app.js
@@ -14,7 +14,7 @@ weatherForm.addEventListener('submit', (e) => {
             if (data.error) {
                 message1.textContent = data.error
             } else {
-                message1.textContent = data.location + data.forecast
+                message1.textContent = data.location + '\n' + data.forecast
             }
         })
     })
